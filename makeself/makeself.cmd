d69de3cf1b10a293175037786137a59f8a75db82
Ken Long <long27km@uw.edu>
2018-07-05 14:47:04 -0700

    updated docker containers, adding some sort of CI/CD to initialize and begin working with gitlab CI, updated makeself command to refer to beta.2, new installer not built

--- makeself/makeself.cmd
+++ makeself/makeself.cmd
@@ -1,1 +1,1 @@
-makeself --notemp --follow install-esaude/ install-esaude-1.0.0-beta.1.run "eSaúde Platform & EMR POC v1.0.0-beta.1" ./install-eSaude.sh
+makeself --notemp --follow install-esaude/ install-esaude-1.0.0-beta.2.run "eSaúde Platform & EMR POC v1.0.0-beta.2" ./install-eSaude.sh
