502f06cec4eb27f0705e8f647f499f2ab18a58ff
Ken Long <long27km@uw.edu>
2018-07-12 23:27:11 +0000

    enabling download of containers, fixed relative path issue with makeself

--- makeself/makeself.cmd
+++ makeself/makeself.cmd
@@ -1,6 +1,12 @@
+path_to_here=$( dirname "${BASH_SOURCE[0]}" )
+
+pushd $path_to_here
+
 . ./install-esaude/get_version.sh
 
 mkdir install-esaude/log -p
 mkdir build -p
 
 makeself --notemp --follow install-esaude/ build/install-esaude-${installer_version}.run "eSaúde Platform & EMR POC v${installer_version}" ./install-eSaude.sh
+
+popd
