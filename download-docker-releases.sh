4b01561d5d7755c029ce96447a53dc72ebc94523
Ken Long <long27km@uw.edu>
2018-07-12 21:51:40 +0000

    updated download for docker .deb pkgs and docker compose, update download esaude containers to use latest from bintray REST API, updated .gitignore to ignore local copies of docker pkgs and compose and esaude containers

--- download-docker-releases.sh
+++ download-docker-releases.sh
@@ -5,3 +5,5 @@ version="18.03.1"
 for codename in ${codenames[@]}; do
 	wget -r -l1 -np -nd -P "$codename" "https://download.docker.com/linux/ubuntu/dists/$codename/pool/stable/amd64/" -A "docker-ce_${version}*.deb"
 done
+
+mkdir -p common && wget "https://github.com/docker/compose/releases/download/1.21.0/docker-compose-Linux-x86_64" -O "common/docker-compose" && chmod +x common/docker-compose
--- pull-pkg-docker-containers.sh
+++ pull-pkg-docker-containers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 interactive=0
 automated=0
-slow_latest=0
 help="-i for interactive (specify versions manually)\n-a for automated (expect env vars to be set)"
+
 while getopts ":ia" opt; do
 	case ${opt} in
 	  i ) interactive=1;;
@@ -32,39 +32,24 @@ if [ $interactive -eq 1 ]; then
 	read latest_poc
 fi
 
+
+
 if [ $automated -eq 1 ]; then
 
-	if [ -z $latest_tomcat ]; then
-		echo -e "\$latest_tomcat is not set.\nIt should be a version number used by the artifactory e.g. 1.4.5"
-	exit_script=""
-	fi
-	if [ -z $latest_mysql ]; then
-		echo -e "\$latest_mysql is not set.\nIt should be a version number used by the artifactory e.g. 1.4.5"
-	exit_script=""
-	fi
-	if [ -z $latest_poc ]; then
-		echo -e "\$latest_poc is not set.\nIt should be a version number used by the artifactory e.g. 2.0.2"
-	exit_script=""
-	fi
+	vars=("latest_mysql" "latest_tomcat" "latest_poc")
+	pkgs=("platform-docker/mysql" "platform-docker/tomcat" "poc-docker/poc")
 
-if [ -e $exit_script ]; then
-	exit 255
-fi
+	index=0
+	for pkg in ${pkgs[@]}; do
+		echo ${vars[$index]}
 
-fi
-
-if [ $slow_latest -eq 1 ]; then
+		
+		eval "${vars[$index]}=`curl https://api.bintray.com/packages/esaude/$pkg/versions/_latest | grep -P '(?<=\"name\":\").*?(?=",)' -o`"
+		
+		let "index++"
+	done
 	
-	#probably the slowest way to find out the most recent version, but the most immediately implementable. there are no "latest" tags? seems like that would be much faster and easier, need to find out why there aren't...
-	sudo docker pull -a esaude-docker-platform-docker.bintray.io/tomcat
-	sudo docker pull -a esaude-docker-platform-docker.bintray.io/mysql
-	sudo docker pull -a esaude-docker-poc-docker.bintray.io/poc
-
-	latest_tomcat=sudo docker images esaude-docker-platform-docker.bintray.io/tomcat --format "{{.Tag}}" | sort -Vr | head -n 1
-	
-	latest_mysql=sudo docker images esaude-docker-platform-docker.bintray.io/mysql --format "{{.Tag}}" | sort -Vr | head -n 1
-	
-	latest_poc=sudo docker images esaude-docker-poc-docker.bintray.io/poc --format "{{.Tag}}" | sort -Vr | head -n 1
+	echo "mysql $latest_mysql tomcat $latest_tomcat poc $latest_poc"
 fi
 
 #remove old containers
