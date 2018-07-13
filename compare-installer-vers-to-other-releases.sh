1c9b69e5f728f4aefbbf59110d9f081754879c32
Ken Long <long27km@uw.edu>
2018-07-13 23:38:45 +0000

    inc version number from latest on bintray when a new version needs to be built for newly published contaienrs

--- compare-installer-vers-to-other-releases.sh
+++ compare-installer-vers-to-other-releases.sh
@@ -2,12 +2,19 @@
 . ./makeself/install-esaude/get_version.sh
 . ./get-latest-container-ver-num.sh
 
-this_releases_versions=`curl -u $BINTRAY_APIUSER:$BINTRAY_APIKEY https://api.bintray.com/packages/esaude/installer/installer/versions/$installer_version --data attribute_values=1`
+this_releases_versions=`curl -u $BINTRAY_APIUSER:$BINTRAY_APIKEY https://api.bintray.com/packages/esaude/installer/installer/versions/_latest --data attribute_values=1`
 
 echo "$this_releases_versions"
 
+new_installer_version=`echo "$this_releases_versions" | jq '.name[]' | sed 's/"//g'`
 POC_Ver=`echo "$this_releases_versions" | jq '.attributes.POC_Ver[]' | sed 's/"//g'`
 Platform_Ver=`echo "$this_releases_versions" | jq 'attributes.Platform_Ver[]' | sed 's/"//g'`
+
+min_ver=`echo $new_installer_version | grep -P '(?<=\.)[0-9]+(?=\.)' -o`
+let "min_ver++"
+new_installer_version=`echo $new_installer_version | sed -P "s/\.[0-9]+\./$min_ver/"`
+
+echo "installer_version=$new_installer_version" > ./makeself/install-esaude/get_version.sh
 
 dpkg --compare-versions "$POC_Ver" ge "$latest_poc"
 poc_req="$?"
