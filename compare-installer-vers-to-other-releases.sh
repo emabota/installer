80248229bbc4c9757e35710fd044604868f2fcf7
Ken Long <long27km@uw.edu>
2018-07-14 00:10:01 +0000

    more verbose check compare version check, cleanedup check for latest versions of containers, slightly more verbose travis script

--- compare-installer-vers-to-other-releases.sh
+++ compare-installer-vers-to-other-releases.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 . ./makeself/install-esaude/get_version.sh
-. ./get-latest-container-ver-num.sh
 
+echo "Querying Bintray REST API"
 this_releases_versions=`curl -u $BINTRAY_APIUSER:$BINTRAY_APIKEY https://api.bintray.com/packages/esaude/installer/installer/versions/_latest --data attribute_values=1`
 
+echo "Response"
 echo "$this_releases_versions"
 
+echo "using jq to parse response"
 new_installer_version=`echo "$this_releases_versions" | jq '.name[]' | sed 's/"//g'`
 POC_Ver=`echo "$this_releases_versions" | jq '.attributes.POC_Ver[]' | sed 's/"//g'`
 Platform_Ver=`echo "$this_releases_versions" | jq '.attributes.Platform_Ver[]' | sed 's/"//g'`
@@ -15,6 +17,8 @@ let "min_ver++"
 new_installer_version=`echo $new_installer_version | sed -r "s/\.[0-9]+\./$min_ver/"`
 
 echo "installer_version=$new_installer_version" > ./makeself/install-esaude/get_version.sh
+
+cat ./makeself/install-esaude/get_version.sh
 
 dpkg --compare-versions "$POC_Ver" ge "$latest_poc"
 poc_req="$?"
--- get-latest-container-ver-num.sh
+++ get-latest-container-ver-num.sh
@@ -5,7 +5,7 @@ pkgs=("platform-docker/mysql" "platform-docker/tomcat" "poc-docker/poc")
 
 index=0
 for pkg in ${pkgs[@]}; do
-	echo ${vars[$index]}
+	#echo ${vars[$index]}
 
 		
 	eval "${vars[$index]}=`curl https://api.bintray.com/packages/esaude/$pkg/versions/_latest | grep -P '(?<=\"name\":\").*?(?=",)' -o`"
