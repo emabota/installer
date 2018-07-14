7feb6d0871cab88b67b09ad7db1a5e4c96684abb
Ken Long <long27km@uw.edu>
2018-07-14 00:16:54 +0000

    even more verbose compare vers check

--- compare-installer-vers-to-other-releases.sh
+++ compare-installer-vers-to-other-releases.sh
@@ -4,13 +4,18 @@
 echo "Querying Bintray REST API"
 this_releases_versions=`curl -u $BINTRAY_APIUSER:$BINTRAY_APIKEY https://api.bintray.com/packages/esaude/installer/installer/versions/_latest --data attribute_values=1`
 
-echo "Response"
-echo "$this_releases_versions"
+echo "Response: $this_releases_versions"
 
 echo "using jq to parse response"
+
 new_installer_version=`echo "$this_releases_versions" | jq '.name[]' | sed 's/"//g'`
+echo "\$new_installer_version==$new_installer_version"
+
 POC_Ver=`echo "$this_releases_versions" | jq '.attributes.POC_Ver[]' | sed 's/"//g'`
+echo "\$POC_Ver==$POC_Ver"
+
 Platform_Ver=`echo "$this_releases_versions" | jq '.attributes.Platform_Ver[]' | sed 's/"//g'`
+echo "\$Platform_Ver==$Platform_ver"
 
 min_ver=`echo $new_installer_version | grep -P '(?<=\.)[0-9]+(?=\.)' -o`
 let "min_ver++"
@@ -25,4 +30,8 @@ poc_req="$?"
 dpkg --compare-versions "$Platform_Ver" ge "$latest_tomcat" 
 plat_req="$?"
 
+echo "\$poc_req=$poc_req, \$plat_req=$plat_req"
+
 export NEW_VERSION=$(( poc_req+plat_req ))
+
+echo "\$NEW_VERSION=$NEW_VERSION"
