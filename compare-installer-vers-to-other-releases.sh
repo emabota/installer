02860786e13995e9fcd4eebcf42b13aff580490c
Ken Long <long27km@uw.edu>
2018-07-16 18:27:20 +0000

    fixed minor and fix ver string building in CI

--- compare-installer-vers-to-other-releases.sh
+++ compare-installer-vers-to-other-releases.sh
@@ -21,14 +21,58 @@ Platform_Ver=`echo "$prev_releases_versions" | jq '.attributes.Platform_Ver[]' |
 echo "\$Platform_Ver==$Platform_Ver"
 
 min_ver=`echo $prev_installer_version | grep -P '(?<=\.)[0-9]+(?=\.)' -o`
-let "min_ver++"
 
 echo "current code-local installer version is $installer_version"
 
-code_fix_ver=`echo $installer_version | grep -P '(?<=\.)[0-9]+|(rc|b|a){1}(?=$)' -o`
-echo "code local fix version is $code_fix_ver"
+split_ver='[0-9]+(-(rc|a|b){1})?'
 
-rel_fix_ver=`echo $prev_installer_version | grep -P '(?<=\.)[0-9]+(?=\.)' -o`
+local_ver_substring="$installer_version"
+rel_ver_substring="$prev_installer_version"
+for i in {1..4}; do
+
+	if [ "$i" -eq 4 ]; then
+		dot="."
+	else
+		dot=""
+	fi
+
+	if [[ $local_ver_substring =~ $split_ver ]]; then
+	
+		if [ "$i" -gt 2 ]; then
+			code_fix_ver="$code_fix_ver$dot${BASH_REMATCH[0]}"
+		fi
+			#code_fix_ver=`echo $installer_version | grep -P '(?<=\.)[0-9]+(\-(rc|b|a){1}(?=$)' -o`
+			local_ver_substring=${local_ver_substring#*${BASH_REMATCH[0]}}
+	else
+		#4th ver is optional, only 3 required
+		if [ "$i" -lt 4 ]; then
+			echo "error parsing code-local version number"
+			exit 1
+		fi
+	fi
+
+
+	if [[ $rel_ver_substring  =~ $split_ver ]]; then
+
+		echo "match ${BASH_REMATCH[0]}"
+
+		if [ "$i" -gt 2 ]; then
+			rel_fix_ver="$rel_fix_ver$dot${BASH_REMATCH[0]}"
+		fi
+
+		rel_ver_substring=${rel_ver_substring#*${BASH_REMATCH[0]}}
+
+#rel_fix_ver=`echo $prev_installer_version | grep -P '(?<=\.)[0-9]+(?=\.)' -o`
+	else
+		#4th ver is optional, only 3 required
+		if [ "$i" -lt 4 ]; then
+			echo "error parsing previous release installer version"
+			exit 1
+		fi
+	fi
+done
+
+echo "code local fix version is $code_fix_ver"
 echo "release fix version is $rel_fix_ver"
 
 dpkg --compare-versions "$rel_fix_ver" ge "$code_fix_ver" 
@@ -40,9 +84,13 @@ fix_ver=$rel_fix_ver
 #if code fix version number is newer, use it instead
 if [ "$code_req" > 0 ]; then
 	fix_ver=$code_fix_ver
+else
+	let "min_ver++"
 fi
 
-new_installer_version=`echo $prev_installer_version | sed -r "s/\.[0-9]+\.([0-9]+|(rc|a|b)){1}/\.$min_ver\./$fix_ver"`
+echo "using .$min_ver.$fix_ver for min.fix ver"
+
+new_installer_version=`echo $prev_installer_version | sed -r "s/\.[0-9]+\.$split_ver.*/\.$min_ver\.$fix_ver/"`
 
 echo "installer_version=\"$new_installer_version\"" > ./makeself/install-esaude/get_version.sh
 
@@ -54,7 +102,7 @@ poc_req="$?"
 dpkg --compare-versions "$Platform_Ver" ge "$latest_tomcat" 
 plat_req="$?"
 
-echo "\$poc_req=$poc_req, \$plat_req=$plat_req"
+echo "\$poc_req=$poc_req, \$plat_req=$plat_req, \$code_req=$code_req"
 
 export NEW_VERSION=$(( poc_req+plat_req+code_req ))
 
